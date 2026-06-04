103463b6ea5d1b588734ce5ac11007f71f846ef4
sizdr <elmejoresmamberroi@gmail.com>
2026-06-04 14:31:42 -0600

    Añadiendo mnodelo Pollo

---
+++ src/modelos/Pollo.java
@@ -0,0 +1,15 @@
+package modelos;
+
+public class Pollo {
+    private String id;
+    private String nombre;
+    private String raza;
+
+    //Edad en meses
+    private int edad;
+    private double peso;
+
+    //Estados posibles: sano, enfermo, en observacion, vacunado
+    private String estado_salud;
+    private boolean activo;
+}
